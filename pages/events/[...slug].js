74afb2b2923594172ad1ae7f7727d7db6687a5d6
William <stewart-w24@hotmail.co.uk>
2021-06-13 17:25:21 +0100

    added firebase link

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -16,7 +16,7 @@ function FilteredEventsPage(props) {
   const filterData = router.query.slug;
 
   const { data, error } = useSWR(
-    'https://nextjs-course-c81cc-default-rtdb.firebaseio.com/events.json'
+    'https://next-js-dummy-backend-ws-default-rtdb.firebaseio.com/events.json'
   );
 
   useEffect(() => {
