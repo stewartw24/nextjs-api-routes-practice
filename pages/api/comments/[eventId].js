045148952979e244f3c7cec4bdb9467a7575b390
William <stewart-w24@hotmail.co.uk>
2021-06-14 22:43:21 +0100

    adding api routes

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,41 @@
+function handler(req, res) {
+    const eventId = req.query.eventId; //the name given to the file between square brackets
+    if(req.method === 'POST') {
+        //add server side validation
+        const {email, name, text} = req.body;
+
+        if(
+            !email.includes('@') || 
+            !name || 
+            name.trim() === '' || 
+            !text || 
+            text.trim() === ''
+            ){
+                res.status(422).json({ message: 'Invalid input.'})
+                return;
+            }
+
+            console.log(email, name, text);
+            const newComment = {
+                id: new Date().toISOString(),
+                email, 
+                name, 
+                text
+            };
+
+            console.log(newComment);
+
+            res.status(201).json({message: 'Added comment', comment: newComment});
+    }
+
+    if(req.method === 'GET'){
+        const dummyList = [
+            {id: 'c1', name: 'Max', text: 'A first comment'},
+            {id: 'c1', name: 'Manu', text: 'A second comment'},
+        ];
+
+        res.status(200).json( {comments: dummyList});
+    }
+}
+
+export default handler;
