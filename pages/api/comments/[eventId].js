5bcd57c9487cbc30a4496b6328387fff82ce4374
William <stewart-w24@hotmail.co.uk>
2021-06-17 23:08:03 +0100

    added comments and error handling

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,5 +1,10 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
     const eventId = req.query.eventId; //the name given to the file between square brackets
+    
+    const client = await MongoClient.connect('mongodb+srv://stewartw24:10736504Abc@cluster0.t6lkr.mongodb.net/events?retryWrites=true&w=majority');
+    
     if(req.method === 'POST') {
         //add server side validation
         const {email, name, text} = req.body;
@@ -17,25 +22,36 @@ function handler(req, res) {
 
             console.log(email, name, text);
             const newComment = {
-                id: new Date().toISOString(),
                 email, 
                 name, 
-                text
+                text,
+                eventId
             };
 
-            console.log(newComment);
+            const db = client.db();
+
+            const result = await db.collection('comments').insertOne(newComment);
+
+            console.log(result);
+
+            newComment.id = result.insertedId;
 
             res.status(201).json({message: 'Added comment', comment: newComment});
     }
 
     if(req.method === 'GET'){
-        const dummyList = [
-            {id: 'c1', name: 'Max', text: 'A first comment'},
-            {id: 'c1', name: 'Manu', text: 'A second comment'},
-        ];
+        const db = client.db();
 
-        res.status(200).json( {comments: dummyList});
+        const document = await db
+            .collection('comments')
+            .find()
+            .sort({ _id:-1 })
+            .toArray();
+
+        res.status(200).json( {comments: document});
     }
+
+    client.close();
 }
 
 export default handler;
--- pages/api/newsLetter.js
+++ pages/api/newsLetter.js
@@ -1,4 +1,20 @@
-function handler(req, res){
+import { MongoClient } from 'mongodb';
+
+async function connectDatabase(){
+    const client = await MongoClient.connect('mongodb+srv://stewartw24:10736504Abc@cluster0.t6lkr.mongodb.net/events?retryWrites=true&w=majority'
+    );
+
+    return client;
+}
+
+async function insertDocument(client, document){
+    const db = client.db();
+
+    await db.collection('newsletter').insertOne(document);
+
+}
+
+async function handler(req, res){
     if(req.method === 'POST'){
         const userEmail = req.body.email;
 
@@ -7,7 +23,24 @@ function handler(req, res){
             return;
         }
 
-        console.log(userEmail);
+        let client;
+
+        try {
+            client = await connectDatabase();
+        } catch (error) {
+            res.status(500).json({message: 'Connecting to the database failed'});
+            return;
+        }
+        
+        try {
+            await insertDocument(client, {email: userEmail});
+            client.close();
+        } catch (error){
+            res.status(500).json({message: 'Inserting data failed'});
+            return;
+        }
+        
+        
         res.status(200).json({message: 'Signed up!'});
     }
 }
